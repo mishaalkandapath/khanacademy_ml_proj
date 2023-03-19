461da9c493d82c968ef98fa23db7e5613f6c255f
mishaalkandapath <mishaalhere@gmail.com>
2023-03-19 19:41:11 -0400

    l2 reg, hyperparam tuning

--- part_a/neural_network.py
+++ part_a/neural_network.py
@@ -9,6 +9,8 @@ import torch.utils.data
 import numpy as np
 import torch
 
+import matplotlib.pyplot as plt
+
 
 def load_data(base_path="data"):
     """ Load the data in PyTorch Tensor.
@@ -101,6 +103,10 @@ def train(model, lr, lamb, train_data, zero_train_data, valid_data, num_epoch):
     optimizer = optim.SGD(model.parameters(), lr=lr)
     num_student = train_data.shape[0]
 
+    #storing data for plotting
+    train_loss_arr = []
+    valid_acc_arr = []
+
     for epoch in range(0, num_epoch):
         train_loss = 0.
 
@@ -115,15 +121,20 @@ def train(model, lr, lamb, train_data, zero_train_data, valid_data, num_epoch):
             nan_mask = np.isnan(train_data[user_id].unsqueeze(0).numpy())
             target[0][nan_mask] = output[0][nan_mask]
 
-            loss = torch.sum((output - target) ** 2.)
+            loss = torch.sum((output - target) ** 2.) + lamb * model.get_weight_norm() #added the weight regularizer,. 
             loss.backward()
 
             train_loss += loss.item()
             optimizer.step()
 
         valid_acc = evaluate(model, zero_train_data, valid_data)
+        train_loss_arr.append(train_loss)
+        valid_acc_arr.append(valid_acc)
         print("Epoch: {} \tTraining Cost: {:.6f}\t "
               "Valid Acc: {}".format(epoch, train_loss, valid_acc))
+    # plot_loss(train_loss_arr, valid_acc_arr)
+
+    
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -154,6 +165,27 @@ def evaluate(model, train_data, valid_data):
         total += 1
     return correct / float(total)
 
+def plot_loss(train_loss, valid_acc):
+    """ Plot the loss and accuracy curves.
+
+    :param train_loss: list
+    :param valid_acc: list
+    :return: None
+    """
+    fig, ax1 = plt.subplots()
+    ax1.plot(train_loss, 'b-')
+    ax1.set_xlabel('Epoch')
+    ax1.set_ylabel('Training Loss', color='b')
+    ax1.tick_params('y', colors='b')
+
+    ax2 = ax1.twinx()
+    ax2.plot(valid_acc, 'r-')
+    ax2.set_ylabel('Validation Accuracy', color='r')
+    ax2.tick_params('y', colors='r')
+
+    fig.tight_layout()
+    plt.savefig("loss.png")
+
 
 def main():
     zero_train_matrix, train_matrix, valid_data, test_data = load_data()
@@ -164,16 +196,32 @@ def main():
     # validation set.                                                   #
     #####################################################################
     # Set model hyperparameters.
-    k = None
-    model = AutoEncoder(train_matrix.shape[1])
+    k = 100
+    
 
     # Set optimization hyperparameters.
-    lr = 0.001
+    lr = 0.1
     num_epoch = 100
-    lamb = None
+    lamb = 0
 
-    train(model, lr, lamb, train_matrix, zero_train_matrix,
-          valid_data, num_epoch)
+    # for k in [10, 50, 100, 200, 500]:
+    #     model = AutoEncoder(train_matrix.shape[1], k)
+    #     print()
+    #     print("k = {}".format(k))
+    #     train(model, lr, lamb, train_matrix, zero_train_matrix,
+    #       valid_data, num_epoch)
+
+    #choosing k = 100 based on validation accuracy of 0.657, testacc 0.6627
+    for lamb in [0.001, 0.01, 0.1, 1]:
+        model = AutoEncoder(train_matrix.shape[1], 100)
+        train(model, lr, lamb, train_matrix, zero_train_matrix, valid_data, k)
+        test_acc = evaluate(model, zero_train_matrix, test_data)
+        print("Test Accuracy: {}".format(test_acc))
+    #the model performs significantly worse in terms of loss, less but still worse in terms of validation accuracy
+
+
+
+    
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
